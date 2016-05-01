674ed19f4e3f588cbeace2a3c7c68d43f08b47c3
Gustavo Nascimento <gusta.nas@gmail.com>
2016-05-01 21:09:30 +0200

    Modify config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host : 'https://gustanas.herokuapp.com'}
-  
   ActionMailer::Base.smtp_settings = {
           :address        => 'smtp.sendgrid.net',
           :port           => '587',
